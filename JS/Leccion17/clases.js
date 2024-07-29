56b30ad489c8b8814b6ee03695ed11080eee8006
Bonino04 <gonza.bonino.2004@gmail.com>
2024-07-29 11:41:01 -0300

    CursoCompleto2

--- JS/Leccion17/clases.js
+++ JS/Leccion17/clases.js
@@ -9,8 +9,30 @@ class Persona{
     set nombre(nombre){
         this._nombre = nombre;
     }
+    get apellido(){
+        this._apellido = apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);//llamar al constructor de la clase padre
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
 }
 
+
 let persona1 = new Persona('Juan' , 'Perez');
-persona1.nombre = 'Juan Carlos'; // set ('Juan Carlos')
-console.log( persona1.nombre); //get nombre
+console.log( persona1); //get nombre
+
+let empleado1 = new Empleado('Maria', 'Jimenez' , 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre);
